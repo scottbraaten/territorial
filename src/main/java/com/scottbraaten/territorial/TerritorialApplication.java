c71d6b13c8f04bd54cb5ff40a4a05e17d868d9cd
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-06-18 22:46:26 -0500

    first commit

---
+++ src/main/java/com/scottbraaten/territorial/TerritorialApplication.java
@@ -0,0 +1,13 @@
+package com.scottbraaten.territorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerritorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerritorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scottbraaten/territorial/TerritorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottbraaten.territorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerritorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
